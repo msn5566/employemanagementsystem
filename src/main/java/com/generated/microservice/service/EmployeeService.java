1a2dcfacfb681c3c0e989aca5d428e553f9b4445
msn5566 <85934258+msn5566@users.noreply.github.com>
2025-07-25 12:15:21 +0530

    feat: Implement endpoint to add new employee details to directory.

---
+++ src/main/java/com/generated/microservice/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.generated.microservice.service;
+
+import com.generated.microservice.dto.EmployeeDTO;
+
+public interface EmployeeService {
+
+    String addEmployee(EmployeeDTO employeeDTO);
+
+}
